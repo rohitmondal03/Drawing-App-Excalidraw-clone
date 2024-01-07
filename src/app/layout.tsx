5cedd2045052526e79ec6f7024afbdb90cd11c5c
Rohit <rohitmondall8000@gmail.com>
2024-01-07 12:09:42 +0530

    small changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next'
 
+import { SITE_AAP_NAME, SITE_DESCRIPTION, SITE_WEBSITE } from '@/config/site'
 import './globals.css'
 
 
 
 export const metadata: Metadata = {
-  title: 'drawing app, but a good one...',
-  description: 'drawing app, but a good one...',
+  title: SITE_DESCRIPTION,
+  description: SITE_DESCRIPTION,
   applicationName: "drww",
   generator: "Next.js",
   authors: {
@@ -14,12 +15,13 @@ export const metadata: Metadata = {
     url: "https://github.com/rohitmondal03"
   },
   openGraph: {
-    siteName: "drww",
-    description: "drawing app, but a good one...",
-    title: "drww",
-    url: WEBSITE,
+    siteName: SITE_AAP_NAME,
+    title: SITE_AAP_NAME,
+    description: SITE_DESCRIPTION,
+    url: SITE_WEBSITE,
   }
 }
+
 
 export default function RootLayout({
   children,
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,1 +1,3 @@
-const WEBSITE= "https://drww.vercel.app/"
+export const SITE_WEBSITE= "https://drww.vercel.app/"
+export const SITE_AAP_NAME= "drww"
+export const SITE_DESCRIPTION= "drawing app, but a good one..."
