ff455eae3989d01710e95caff2415c1b72de4d3e
Rohit <rohitmondall8000@gmail.com>
2024-01-07 12:19:26 +0530

    added og and twitter image

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -4,7 +4,6 @@ import { ImageResponse } from 'next/og'
 export const runtime = 'edge'
 
 // Image metadata
-export const alt = 'About Acme'
 export const size = {
   width: 1200,
   height: 630,
@@ -14,11 +13,6 @@ export const contentType = 'image/png'
 
 // Image generation
 export default async function Image() {
-  // Font
-  const interSemiBold = fetch(
-    new URL('./Inter-SemiBold.ttf', import.meta.url)
-  ).then((res) => res.arrayBuffer())
-
   return new ImageResponse(
     (
       // ImageResponse JSX element
@@ -42,14 +36,6 @@ export default async function Image() {
       // For convenience, we can re-use the exported opengraph-image
       // size config to also set the ImageResponse's width and height.
       ...size,
-      fonts: [
-        {
-          name: 'Inter',
-          data: await interSemiBold,
-          style: 'normal',
-          weight: 400,
-        },
-      ],
     }
   )
 }
